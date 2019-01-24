7e7aef75a5ad60a8d3645ea16400259f997b91a2
quochung <quochung@quochungs-Mac-mini.local>
2019-01-24 12:00:17 +0700

    cap nhat giao dien

--- index.js
+++ index.js
@@ -13,20 +13,20 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/voipmenucall.html');
 })
 
-var options = {
-    cert: fs.readFileSync('/etc/letsencrypt/live/shoppingnow.xyz/fullchain.pem'),
-    key: fs.readFileSync('/etc/letsencrypt/live/shoppingnow.xyz/privkey.pem')
-};
+// var options = {
+//     cert: fs.readFileSync('/etc/letsencrypt/live/shoppingnow.xyz/fullchain.pem'),
+//     key: fs.readFileSync('/etc/letsencrypt/live/shoppingnow.xyz/privkey.pem')
+// };
 
-httpsServer = https.createServer(options, app);
+// httpsServer = https.createServer(options, app);
 
-httpsServer.listen(port, function() {
-    console.log('Audio call running port', port); 
-})
-
-// httpServer = http.Server( app);
-
-// httpServer.listen(port, function(){
-//     console.log('Audio call running port', port);
+// httpsServer.listen(port, function() {
+//     console.log('Audio call running port', port); 
 // })
+
+httpServer = http.Server( app);
+
+httpServer.listen(port, function(){
+    console.log('Audio call running port', port);
+})
 
